bc6dc52d282a93d53bdd28349fdfb17766376b51
Andreas Hultgren <me@andreashultgren.se>
2014-03-23 17:12:48 +0100

    404

---
+++ src/wordpress/wp-content/themes/sw2/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<section class="main row global-width clearfix" id="main">
+  <article <?php post_class(); ?>>
+    <h1>404</h1>
+    <p>
+      Sidan kunde inte hittas.
+    </p>
+  </article>
+</section>
+
+<?php get_footer(); ?>
